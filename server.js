59aca127f8a40cf01c01c977054ebbd028237d83
ayuhsya <ayushm.kgp@gmail.com>
2017-01-14 15:24:41 +0530

    Sat Jan 14 15:24:41 IST 2017

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.json())
 
-app.listen(process.env.PORT || 3000);
+var port = process.env.PORT || 3000;
+app.listen(port);
 
 var core = require('./routes/core');
 app.use('/',core);
 
-console.log("HitchBike running on ", config.port);
+console.log("HitchBike running on ", port);
