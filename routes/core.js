5dfe74a364a0042b480a4126944d3521a917e287
ayuhsya <ayushm.kgp@gmail.com>
2017-01-14 21:12:56 +0530

    Sat Jan 14 21:12:56 IST 2017

--- routes/core.js
+++ routes/core.js
@@ -95,7 +95,7 @@ core.get('/getusers', function(req, res, next){
 core.post('/toggleavailability', function(req, res, next){
   console.log("Updating availability for",req.body);
 
-  var availability = (req.body.availability === 'true') ? 1 : 0;
+  var availability = 1; //(req.body.availability == 'true') ? 1 : 0;
   console.log("Set availability", availability);
   var connection = new Connection(config.sqlserver);
   connection.on('connect', function(err) {
