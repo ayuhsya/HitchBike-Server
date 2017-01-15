0e2703c01024bacbb32a887f8cea10bafb77fb69
ayuhsya <ayushm.kgp@gmail.com>
2017-01-15 10:55:06 +0530

    Sun Jan 15 10:55:06 IST 2017

--- helpers.js
+++ helpers.js
@@ -1,6 +1,3 @@
-var events = require('events');
-var eventEmitter = events.EventEmitter();
-
 module.exports = {
   compareFunction: function(user1, user2){return (user1.geoDistance > user2.geoDistance) ? 1 : ((user1.geoDistance < user2.geoDistance) ? -1 : 0)},
 
